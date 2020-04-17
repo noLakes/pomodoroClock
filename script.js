4568d1d718cd832cc3d5853ce7167a05d8670795
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-04-17 00:14:11 -0400

    style, event listeners for buttons, variables linking to DOM elements

---
+++ script.js
@@ -0,0 +1,36 @@
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
+const pomoButton = document.querySelector("#pomoButton");
+const shortBreakButton = document.querySelector("#shortBreakButton");
+const longBreakButton = document.querySelector("#longBreakButton");
+const timer = document.querySelector("#timer");
+
+window.addEventListener('load', (event) => {
+    start.addEventListener('click', function () {
+
+    });
+
+    stop.addEventListener('click', function () {
+
+    });
+
+    reset.addEventListener('click', function () {
+
+    });
+
+    pomoButton.addEventListener('click', function() {
+
+    });
+
+    shortBreakButton.addEventListener('click', function () {
+
+    });
+
+    longBreakButton.addEventListener('click', function() {
+
+    });
+});
+
+
+
