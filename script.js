e1fc5825ce260fad80a2359b891e6c2a95e46d09
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-04-18 16:55:37 -0400

    added timer logic and further button styling

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ const shortBreakButton = document.querySelector("#shortBreakButton");
 const longBreakButton = document.querySelector("#longBreakButton");
 const timer = document.querySelector("#timer");
 
+const pomoTime = [25]; 
+const shortBreakTime = [5];
+const longBreakTime = [10];
+
 window.addEventListener('load', (event) => {
     start.addEventListener('click', function () {
-
+        timeToCount(pomoTime[0]);
     });
 
     stop.addEventListener('click', function () {
@@ -31,6 +35,40 @@ window.addEventListener('load', (event) => {
 
     });
 });
+
+let timeStart = new Date();
+
+function updateTime() {
+    timeStart = new Date();
+}
+
+function timeToCount(min) {
+    updateTime();
+    timerText(min, 0);
+    let timer = setInterval(() => {
+        let d = new Date();
+        timerText(min, (d % timeStart));
+        if ((d % timeStart) >= (min * 60000)) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+function timerText(targetTime, timeElapsed) {
+    targetTime *= 60000;
+    let timeToPrint = targetTime - timeElapsed;
+    if (timeToPrint < 1) {
+        timeToPrint = 0;
+    }
+    timer.textContent = msToMinSec(timeToPrint);
+}
+
+function msToMinSec(ms) {
+    let minutes = Math.floor(ms / 60000);
+    let seconds = ((ms % 60000) / 1000).toFixed(0);
+    return (seconds == 60 ? (minutes+1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
+}
+
 
 
 
